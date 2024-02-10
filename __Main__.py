e927c3ae56f08a307b1f69e63ca0babc0e6ca06a
Officialpolomar <omarmyla@gmail.com>
2024-02-10 16:56:11 -0500

    New function

--- __Main__.py
+++ __Main__.py
@@ -1,3 +1,17 @@
 import mysql.connector
 from mysql.connector import error
 import pandas as pd
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
